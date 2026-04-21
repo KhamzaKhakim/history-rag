d598f3aea1ab776debbd9596db0de25edc4d43fd
Khamza Khakim <hamzir.04@gmail.com>
2026-04-21 22:54:11 +0500

    Setup

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer() {
+  return <div className="h-24 border-b-2 border-black"></div>;
+}
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <div className="h-24 border-b-2 border-black"></div>;
+}
